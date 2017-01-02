26a4d441c028a60cd4e9786cde18621a316a7706
joncoder <jon@minedminds.org>
2017-01-02 15:25:07 -0500

    refactor into a class

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,11 +1,20 @@
-def generate_minimum_coins(cents)
-	change = {}
-	coins = {quarters: 25, dimes: 10, nickles: 5, pennies: 1}
-	coins.each do |coin, value|	
-		if cents >= value
-			change[coin] = cents/value
-			cents = cents%value
+class CoinChanger
+
+	attr_accessor :change
+	attr_reader :coins
+
+	def initialize()
+		@change = {}
+		@coins = {quarters: 25, dimes: 10, nickles: 5, pennies: 1}
+	end
+
+	def generate_minimum_coins(cents)
+		coins.each do |coin, value|	
+			if cents >= value
+				@change[coin] = cents/value
+				cents = cents%value
+			end
 		end
 	end
-	change
+
 end
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -2,39 +2,53 @@ require 'minitest/autorun'
 require_relative 'coin_changer.rb'
 
 class TestCoinChanger < Minitest::Test 
+
+	def setup
+		@coins = CoinChanger.new
+	end
+
 	def test_zero_cents_returns_zero_coins
-		assert_equal({}, generate_minimum_coins(0))
+		@coins.generate_minimum_coins(0)
+		assert_equal({}, @coins.change)
 	end
 
 	def test_one_cent_returns_one_penny
-		assert_equal({pennies: 1}, generate_minimum_coins(1))
+		@coins.generate_minimum_coins(1)
+		assert_equal({pennies: 1}, @coins.change)
 	end
 
 	def test_two_cents_returns_two_pennies
-		assert_equal({pennies: 2}, generate_minimum_coins(2))
+		@coins.generate_minimum_coins(2)
+		assert_equal({pennies: 2}, @coins.change)
 	end
 
 	def test_five_cents_returns_one_nickle
-		assert_equal({nickles: 1}, generate_minimum_coins(5))
+		@coins.generate_minimum_coins(5)
+		assert_equal({nickles: 1}, @coins.change)
 	end
 
 	def test_six_cents_returns_one_nickle_one_penny
-		assert_equal({nickles: 1, pennies: 1}, generate_minimum_coins(6))
+		@coins.generate_minimum_coins(6)
+		assert_equal({nickles: 1, pennies: 1}, @coins.change)
 	end
 
 	def test_ten_cents_returns_one_dime
-		assert_equal({dimes: 1}, generate_minimum_coins(10))
+		@coins.generate_minimum_coins(10)
+		assert_equal({dimes: 1}, @coins.change)
 	end
 
 	def test_17_cents_returns_one_dime_one_nickle_two_pennies
-		assert_equal({dimes: 1, nickles: 1, pennies: 2}, generate_minimum_coins(17))
+		@coins.generate_minimum_coins(17)
+		assert_equal({dimes: 1, nickles: 1, pennies: 2}, @coins.change)
 	end
 
 	def test_20_cents_returns_two_dimes
-		assert_equal({dimes: 2}, generate_minimum_coins(20))
+		@coins.generate_minimum_coins(20)
+		assert_equal({dimes: 2}, @coins.change)
 	end
 
 	def test_92_cents_returns_3_quarters_1_dimes_1_nickle_2_pennies
-		assert_equal({quarters: 3, dimes: 1, nickles: 1, pennies: 2}, generate_minimum_coins(92))
+		@coins.generate_minimum_coins(92)
+		assert_equal({quarters: 3, dimes: 1, nickles: 1, pennies: 2}, @coins.change)
 	end
 end
